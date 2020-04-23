65d6266924c307078e37b81e4bbb3bb5ec1dc6d2
Romain Crestey <rcrestey@gmail.com>
2020-04-23 13:52:11 +0200

    fix: fill basic component

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div />, document.getElementById('root'));
+ReactDOM.render(<div>Coucou</div>, document.getElementById('root'));
