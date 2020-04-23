97781a6d2ad0fbf00b32e24090d0af0f3935bd1f
Frank Bessou <frank.bessou@logilab.fr>
2020-04-23 15:11:10 +0200

    The cake is a lie 🍰

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Coucou</div>, document.getElementById('root'));
+
+interface Personne {
+    nom: string;
+    age: number;
+}
+
+type PersonneProps = Personne;
+
+function Personne(props: PersonneProps) { 
+    const {nom, age} = props;
+    return (
+        <div className="personne">
+            <div className="nom">
+                {nom}
+            </div>
+            <div className="">
+                {age}
+            </div>
+        </div>
+    );
+}
+
+interface ColocProps {
+    personnes: Personne[]
+}
+
+function Coloc({personnes}: ColocProps) {
+    return (
+        <>
+            {
+                personnes.map((personne, i) => 
+                    <Personne key={i} nom={personne.nom} age={personne.age}/>)
+            }
+        </>
+    );
+}
+
+const personnes: Personne[] = [{
+    nom: "Frank",
+    age: 25,
+}, {
+    nom: "Toto",
+    age: 21
+}]
+
+ReactDOM.render((
+    <div>
+        <Coloc personnes={personnes} />
+    </div>),
+    document.getElementById('root'));
