987ec20cdb424b3fb5697c03724bae31e0b310bc
Noe Gaumont <ngaumont@logilab.fr>
2020-04-23 15:09:13 +0200

    Premier essai coloc

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>Coucou</div>, document.getElementById('root'));
+type PersonProps = {
+  nom: string;
+  age: number;
+}
+
+const Person = (props: PersonProps) => {
+  const {nom, age} = props;
+  return (
+    <div className='Person'>
+      <div className='nom'>{nom}</div>
+      <div className='age'>{age}</div>
+    </div>
+  );
+}
+
+type ColocProps = {
+  members:PersonProps[];
+}
+
+const Coloc = ({members}: ColocProps) => {
+  return (
+    <div>
+    {members.map(x => <Person {...x} key={x.nom}/>)}
+    </div>
+  );
+}
+
+const personnes: PersonProps[] = [
+  {
+    age:29,
+    nom:'Noé'
+  },
+  {
+    age:33,
+    nom:'Kentin'
+  }
+]
+
+ReactDOM.render(
+  <Coloc members={personnes} />,
+  document.getElementById('root')
+);
