00e8d4f22bab0b7b10f459bd804f4c8509e42d14
Romain Crestey <rcrestey@gmail.com>
2020-05-25 12:52:02 +0200

    add hooks

---
+++ src/AppHook.tsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect, useMemo } from 'react';
+
+import fetchJson from './fetchJson';
+import AuthorFilter from './AuthorFilter';
+import Filter from './Filter';
+import { ITweet } from './Tweet';
+import TweetList from './TweetList';
+
+interface AppProps {
+  urls: string[];
+}
+
+function App(props: AppProps) {
+  const [isFr, setIsFr] = useState(false);
+  const [tweets, setTweets] = useState<ITweet[] | undefined>();
+  const [selectedAuthor, setSelectedAuthor] = useState<string | undefined>();
+
+  useEffect(() => {
+    const { urls } = props;
+    const myPs = urls.map(fetchJson);
+
+    Promise.all(myPs).then(data => {
+      const tweets = data.flat();
+      setTweets(tweets);
+    });
+  }, []);
+
+  function toggleIsFr() {
+    setIsFr(isFr => !isFr);
+  }
+
+  function setAuthor(name: string) {
+    setSelectedAuthor((a: string | undefined) => {
+      const newAuthor = a === name ? undefined : name;
+
+      return newAuthor;
+    });
+  }
+
+  const authors = useMemo(
+    () => [...new Set(tweets?.map(tweet => tweet.user.name))],
+    [tweets],
+  );
+
+  let tweetsToDisplay = isFr
+    ? tweets?.filter(tweet => tweet.lang === 'fr')
+    : tweets;
+
+  tweetsToDisplay = selectedAuthor
+    ? tweetsToDisplay?.filter(tweet => tweet.user.name === selectedAuthor)
+    : tweetsToDisplay;
+
+  const filterLabel = isFr ? 'To All' : 'To Fr';
+
+  const authorButtons = authors?.map(author => (
+    <AuthorFilter
+      key={author}
+      label={author}
+      handleClick={() => setAuthor(author)}
+    />
+  ));
+
+  return (
+    <div className='App'>
+      <Filter label={filterLabel} handleClick={toggleIsFr} />
+      <div className='authors'>{authorButtons}</div>
+      {tweetsToDisplay ? (
+        <TweetList tweets={tweetsToDisplay} />
+      ) : (
+        <p>Chargement</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './AppHook';
 
 const urls = [
   'https://raw.githubusercontent.com/iOiurson/formation/correction/data/tweets.json',
